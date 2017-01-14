e6eb141f377ea4f43b3544940527a1f3bb75f9fd
Extreeeme <martindcontact@gmail.com>
2017-01-14 23:18:50 +0100

    Ajout de l'animation propre de la deuxième page

--- js/script.js
+++ js/script.js
@@ -1,21 +1,16 @@
-var compteur = 1;
+
 
 document.addEventListener('keyup',  function(){
-  if( document.getElementById('page2') && event.keyCode==40 && (compteur==1|| compteur==3)){
-    document.getElementById('page2').id = "page2-cacher";
-    compteur++;
-  }
-  else if( document.getElementById('page2-cacher') && event.keyCode==38){
+  if( document.getElementById('page2-cacher') && event.keyCode==40){
     document.getElementById('page2-cacher').id = "page2";
-    compteur--;
+    document.getElementById('divTransparente').id = 'divTransparente-cacher';
+
   }
-   else if( document.getElementById('page3') && event.keyCode==40 &&(compteur==2|| compteur==4)) {
-      document.getElementById('page3').id = "page3-cacher";
-      compteur++;
+  else if( document.getElementById('page2') && event.keyCode==38){
+    document.getElementById('page2').id = "page2-cacher";
+    document.getElementById('divTransparente-cacher').id = 'divTransparente';
+
   }
-   else if( document.getElementById('page3-cacher') && event.keyCode==38 ){
-    document.getElementById('page3-cacher').id = "page3";
-    compteur--;
-  }
+
  
 });
