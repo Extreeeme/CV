99f9d3038c547405a0bf8e1cac486ac375651ddb
Extreeeme <martindcontact@gmail.com>
2017-01-15 00:52:33 +0100

    Ajout des flèches

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 
 
 document.addEventListener('keyup',  function(){
-  if( document.getElementById('page2-cacher') && event.keyCode==40){
+  if( document.getElementById('page2-cacher') && event.keyCode==39){
     document.getElementById('page2-cacher').id = "page2";
     document.getElementById('divTransparente').id = 'divTransparente-cacher';
     document.getElementById('competences-cacher').id = 'competences';
-
   }
-  else if( document.getElementById('page2') && event.keyCode==38){
+  else if( document.getElementById('page2') && event.keyCode==37){
     document.getElementById('page2').id = "page2-cacher";
     document.getElementById('divTransparente-cacher').id = 'divTransparente';
     document.getElementById('competences').id = 'competences-cacher';
-
   }
-
- 
 });
