8960f67f89f8d95a0a77dc3d83a79ae965084ecb
Extreeeme <martindcontact@gmail.com>
2017-01-14 23:58:55 +0100

    Page 2 DONE

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ document.addEventListener('keyup',  function(){
   if( document.getElementById('page2-cacher') && event.keyCode==40){
     document.getElementById('page2-cacher').id = "page2";
     document.getElementById('divTransparente').id = 'divTransparente-cacher';
+    document.getElementById('competences-cacher').id = 'competences';
 
   }
   else if( document.getElementById('page2') && event.keyCode==38){
     document.getElementById('page2').id = "page2-cacher";
     document.getElementById('divTransparente-cacher').id = 'divTransparente';
+    document.getElementById('competences').id = 'competences-cacher';
 
   }
 
