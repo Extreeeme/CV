668fc0f642a3084b9b4246d43bffe1a3ca259a06
Extreeeme <martindcontact@gmail.com>
2017-01-15 15:08:56 +0100

    Ajout de la page n°3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 
 
 document.addEventListener('keyup',  function(){
-  if( document.getElementById('page2-cacher') && event.keyCode==39){
+  if( document.getElementById('page2-cacher') && document.getElementById('page3-cacher') && event.keyCode==39){          //Flèche de droite
     document.getElementById('page2-cacher').id = "page2";
     document.getElementById('divTransparente').id = 'divTransparente-cacher';
     document.getElementById('competences-cacher').id = 'competences';
   }
-  else if( document.getElementById('page2') && event.keyCode==37){
+  else if( document.getElementById('page2') && document.getElementById('page3-cacher') && event.keyCode==37){            //Flèche de gauche
     document.getElementById('page2').id = "page2-cacher";
     document.getElementById('divTransparente-cacher').id = 'divTransparente';
     document.getElementById('competences').id = 'competences-cacher';
+  }
+  else if(document.getElementById('page2') && document.getElementById('page3-cacher') && event.keyCode==39){
+    document.getElementById('page3-cacher').id = "page3";
+    document.getElementById('page2').id = "page2-cacher";
+    document.getElementById('competences').id = 'competences-cacher';
+  }
+  else if(document.getElementById('page2-cacher') && document.getElementById('page3') && event.keyCode==37){
+    document.getElementById('page3').id = "page3-cacher";
+    document.getElementById('page2-cacher').id = "page2";
+    document.getElementById('competences-cacher').id = 'competences';
   }
 });
